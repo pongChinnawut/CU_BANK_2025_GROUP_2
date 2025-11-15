91260a910cda168e9e44ff4e04b8ca17678454ab
Pong <chinnawut@MacBook-Air-khxng-Pong.local>
2025-11-15 23:43:40 +0700

    remove credential

--- frontend/tests/helpers/database/mongoose.helper.ts
+++ frontend/tests/helpers/database/mongoose.helper.ts
@@ -13,4 +13,5 @@ export const disconnectDB = async () => {
   await mongoose.disconnect();
 };
 
-export const mongoUrl = "mongodb+srv://chinnawutkpong_db_user:HYK71Rx1ySuphpuS@cubankclustertest.s9bsp9r.mongodb.net/?appName=CUBankClusterTest";
+export const mongoUrl =
+  "mongodb+srv://{{username}}:{{password}}@cubankclustertest.s9bsp9r.mongodb.net/?appName=CUBankClusterTest";
